111ddd4000a1726f67edaa17d546504237101283
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-04-30 20:23:59 +0530

    added button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@radix-ui/themes";
 
 
 export default function Home() {
   return (
-    <div className="">Hello world</div>
+    <div className="">
+      <h1>Hello world</h1>
+      <Button className="border border-amber-100 hover:cursor-pointer">click me</Button>
+      </div>
   )
 }
