5a29a6df73dd737c18a1ed2dc5c99712feda1354
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-01 13:38:06 +0530

    frontend zod validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,20 +1,16 @@
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod'
+import { createIssueSchema } from "../../validationSchemas";
 
-const createIssueSchema = z.object({
-    title:z.string().min(1,'Title is required.').max(255),
-    description: z.string().min(1,'Description is required.')
-})
-export async function POST(request:NextRequest){
-const body = await request.json()
-const validation = createIssueSchema.safeParse(body)
-if(!validation.success){
-    return NextResponse.json(validation.error.format(),{status:400})
-}
-const newIssue = await prisma.issue.create({
-    data:{title:body.title, description:body.description}
-})
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
 
-return NextResponse.json(newIssue,{status:201})
+  return NextResponse.json(newIssue, { status: 201 });
 }
--- app/issues/newIssue/page.tsx
+++ app/issues/newIssue/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 // anything which is doing ssr can be done like this so it not cause any error
 import dynamic from 'next/dynamic';
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
@@ -10,15 +10,17 @@ import "easymde/dist/easymde.min.css";
 import {Controller, useForm} from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import {zodResolver} from '@hookform/resolvers/zod'
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface Issueform{
-  title:string,
-  description:string
-}
+type Issueform = z.infer<typeof createIssueSchema> 
 const page = () => {
   const [error,setError] = useState<string>('')
   const router = useRouter()
-  const {register,control,handleSubmit} = useForm<Issueform>()
+  const {register,control,handleSubmit,formState:{errors}} = useForm<Issueform>({
+    resolver: zodResolver(createIssueSchema)
+  })
   return (
     <div className='max-w-xl'>
       {error&&<Callout.Root color='red' className='mb-5'>
@@ -34,12 +36,14 @@ const page = () => {
       }
     })}>
         <TextField.Root variant='classic' placeholder='Title' className='p-2 w-full' {...register('title')}/>
+        {errors.title&&<Text color='red' as='p'>{errors.title.message}</Text>}
         <Controller
         name='description'
         control={control}
         render={({field})=>
         <SimpleMDE placeholder='describe the issue...' className='' {...field}/>
         }/>
+        {errors.description&&<Text color='red' as='p'>{errors.description.message}</Text>}
         <Button>Submit New Issue</Button>
     </form>
     </div>
