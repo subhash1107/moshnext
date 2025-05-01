5ee70fcba1e1ec438e8f377c80dac6debbd00fb2
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-01 11:02:11 +0530

    added a md editor

--- app/issues/newIssue/page.tsx
+++ app/issues/newIssue/page.tsx
@@ -1,11 +1,18 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+'use client'
 import React from 'react'
+import { Button, TextField } from '@radix-ui/themes'
+// anything which is doing ssr can be done like this so it not cause any error
+import dynamic from 'next/dynamic';
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+import "easymde/dist/easymde.min.css";
 
 const page = () => {
   return (
     <div className='max-w-xl space-y-5'>
         <TextField.Root variant='classic' placeholder='Title' className='p-2 w-full'/>
-        <TextArea placeholder='describe the issue...' size='3' className='p-2'/>
+        <SimpleMDE placeholder='describe the issue...' className='p-2'/>
         <Button>Submit New Issue</Button>
     </div>
   )
