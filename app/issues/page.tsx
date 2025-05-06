73402454bf88f28c8ba803e22b0fd2026a4bae75
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-06 10:55:02 +0530

    showed issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,49 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { prisma } from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
-    <div>
-      <Button className="border border-amber-100 hover:cursor-pointer"><Link href='issues/newIssue'>New Issue</Link></Button>
+    <div className="">
+      <div className="mb-5">
+        <Button className="border border-amber-100 hover:cursor-pointer">
+          <Link href="issues/newIssue">New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created At
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
