c844b0dc79b763696fc7f800bb2b016001a126e8
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-07 12:28:44 +0530

    build patch api for issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ErrorMessage, Spinner } from "@/app/components";
 import { Issue } from "@/app/generated/prisma";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
@@ -17,7 +17,7 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   loading: () => <Spinner />,
 });
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 const IssueForm = ({issue}:{issue?:Issue}) => {
   const [error, setError] = useState<string>("");
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -28,7 +28,7 @@ const IssueForm = ({issue}:{issue?:Issue}) => {
     handleSubmit,
     formState: { errors },
   } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema),
+    resolver: zodResolver(issueSchema),
   });
   const onSubmit = handleSubmit(async (data) => {
     try {
