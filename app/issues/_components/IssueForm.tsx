c814ef064ee677cd8a440c2b6f42a20814e87fdd
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-07 13:06:06 +0530

    fix cache

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -39,6 +39,7 @@ const IssueForm = ({issue}:{issue?:Issue}) => {
       await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setIsSubmitting(true);
       setError("An Unexpected Error Happened.");
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -45,4 +45,6 @@ const page = async () => {
   );
 };
 
+export const dynamic = 'force-dynamic'
+
 export default page;
