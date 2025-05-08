2f4b887fdfb0da16b458ec4826a64e84f373513d
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-08 10:32:43 +0530

    built api to delete issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,7 +1,9 @@
 import { issueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { Value } from "@radix-ui/themes/components/data-list";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import { isUint16Array } from "util/types";
 
 export async function PATCH(
   request: NextRequest,
@@ -26,4 +28,20 @@ export async function PATCH(
   });
 
   return NextResponse.json(updateIssue)
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where:{id:parseInt(params.id)}
+  })
+  if(!issue)
+    return NextResponse.json({error:'Issue Not Found'},{status:404})
+  await prisma.issue.delete({
+    where:{id:issue?.id}
+  })
+
+  return NextResponse.json({})
 }
