1a47fd2753a6dbe62cccc946f3fbd24a330dec33
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-11 01:22:32 +0530

    populate the asignee select component

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -1,18 +1,35 @@
-import { Select } from '@radix-ui/themes'
-import React from 'react'
+"use client";
+
+import { User } from "@/app/generated/prisma";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AsigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+      return;
+    };
+    fetchUsers();
+  }, []);
   return (
     <Select.Root>
-        <Select.Trigger placeholder='Assign...'/>
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                <Select.Item value='1'>Subhash</Select.Item>
-            </Select.Group>
-        </Select.Content>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AsigneeSelect
+export default AsigneeSelect;
