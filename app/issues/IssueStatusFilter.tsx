91fd77828789ce3c2ad12df81f615a8270a54463
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-14 19:28:01 +0530

    fix filtering bugs

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,32 +1,41 @@
-'use client'
-import { Select } from '@radix-ui/themes'
-import React from 'react'
-import { Status } from '../generated/prisma'
-import { useRouter } from 'next/navigation'
+"use client";
+import { Select } from "@radix-ui/themes";
+import React from "react";
+import { Status } from "../generated/prisma";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const statuses:{label:string, value?: Status}[] = [
-    {label:'All'},
-    {label:'Open', value:'OPEN'},
-    {label:'In Progress', value:'IN_PROGRESS'},
-    {label:'Closed', value:'CLOSED'},
-]
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
 const IssueStatusFilter = () => {
-    const router = useRouter();
+  const router = useRouter();
+  const searchParams = useSearchParams();
   return (
-    <Select.Root onValueChange={(status)=>{
-        const query = status === 'all'?'':`?status=${status}`
-        router.push(`/issues${query}`)
-    }}>
-        <Select.Trigger placeholder='Filter by status...'/>
-        <Select.Content>
-             {statuses.map(status=>(
-               <Select.Item key={status.label} value={status.value || 'all'}>
-                {status.label}
-               </Select.Item>
-             ))}
-        </Select.Content>
-    </Select.Root>
-  )
-}
+    <Select.Root
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
 
-export default IssueStatusFilter
+        const query = params.size ? "?" + params.toString() : "";
+        router.push(`/issues${query}`);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.label} value={status.value || "all"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
