da9f93c4cc973fec684a94d4d9c900f126b6fcf8
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-14 20:15:06 +0530

    built the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Pagination from "./components/Pagination";
 
 
 export default function Home() {
   return (
     <div className="">
-      <h1>
-        {/* <img src='https://lh3.googleusercontent.com/a/ACg8ocIVFGiEKRT-BdMlw9AwD44yULWdia7WcrJ0bmnPEzTIRFttm4I=s96-c'/> */}
-      </h1>
+      <Pagination pageSize={5} currentPage={4} itemCount={20}/>
       </div>
   )
 }
