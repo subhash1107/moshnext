022739eb9fddda7e19329dffafc2c0e2f76eb68d
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-19 17:02:53 +0530

    promise dynamic routes in front end

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -3,28 +3,30 @@ import { notFound } from "next/navigation";
 import IssueFormWrapper from "./IssueFormWrapper";
 
 interface Props {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 const EditIssuePage = async ({ params }: Props) => {
+  const { id } = await params;
   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: parseInt(id) },
   });
   if (!issue) notFound();
   return <IssueFormWrapper issue={issue} />;
 };
 
-export async function generateMetadata({params}:Props){
- const issue = await prisma.issue.findUnique({where:{id:parseInt(params.id)}})
- if(!issue){
-  return{
-    title:'Issue not found'
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) } });
+  if (!issue) {
+    return {
+      title: "Issue not found",
+    };
   }
- } 
 
- return{
-  title:`${issue.title} edit`,
-  description:issue.description.slice(0,160)
- }
+  return {
+    title: `${issue.title} edit`,
+    description: issue.description.slice(0, 160),
+  };
 }
 
 export default EditIssuePage;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -9,13 +9,14 @@ import authOption from "@/app/auth/authOption";
 import AsigneeSelect from "./AsigneeSelect";
 import { cache } from "react";
 interface Props {
-  params: { id: string };
+  params: Promise< {id: string} >;
 }
 const fetchIssue = cache((issueId:number)=> prisma.issue.findUnique({where:{id:issueId}}))
 const IssueDetailPage = async ({ params }: Props) => {
-  if (isNaN(parseInt(params.id))) notFound();
+  const {id} = await params
+  if (isNaN(parseInt(id))) notFound();
   const session = await getServerSession(authOption);
-  const issue = await fetchIssue(parseInt(params.id))
+  const issue = await fetchIssue(parseInt(id))
 
   if (!issue) notFound();
   return (
@@ -37,7 +38,8 @@ const IssueDetailPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({params}:Props){
- const issue = await fetchIssue(parseInt(params.id))
+  const {id} = await params
+ const issue = await fetchIssue(parseInt(id))
  if(!issue){
   return{
     title:'Issue not found'
