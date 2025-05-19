0a451ec0829b88af8cedaf78713ed94bb02c10f8
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-05-19 16:27:25 +0530

    hh

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -42,9 +42,9 @@ export async function PATCH(
 
 export async function DELETE(
   request: NextRequest,
-  context: { params:{id:string} }
+  context: { params: { id: string } }
 ) {
-  const {id} = context.params
+  const { id } = context.params;
   const server = await getServerSession(authOption);
   if (!server) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({
